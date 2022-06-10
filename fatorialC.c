1cdd3895b0538b8f40654265346e1abbb8d094e2
Paulo Cesar Silva <paulinhobilingue@gmail.com>
2022-06-10 20:22:39 -0300

    master

--- fatorialC.c
+++ fatorialC.c
@@ -1,10 +1,30 @@
 /* 5Qs do algortimo?
-1 - Qual são dos dados de entrada necessários: numero de 1-12
-2 - O que devo fazer  com estes dados: comprar com o calendario e fornecer o mes correspondente
-3 - Quais as restrições do problema: um numero diferente de 1-12
-4 - Qual é o resultado esperado: fornecer o mes correspondente
-5 - Qual é a sequência de passos necessários para este fim:  
-    receber um numero 
-    criar um case 1-12 com os respectivos meses
-    imprimir na tela o mes correspondente 
+1 - Qual são dos dados de entrada necessários: Um numero inteiro int
+2 - O que devo fazer  com estes dados: calcular o fatorial usando um multplicador
+3 - Quais as restrições do problema: um numero que não seja inteiro
+4 - Qual é o resultado esperado: calcular o fatorial de um numero inteiro
+5 - Qual é a sequência de passos necessários para este fim:
+    Receba um numero - num
+    Crie um for, o numero recebido sera  a quantidade de vezes que o laço sera realizado
+        for (i = 1, i >= num, num++ )
+            fat = num*num;
+    Apresente o resultado
 */
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, fat, i;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    fat = 1;
+    for (i = 1; i <= num; i++)
+    {
+        fat = fat * i;
+    }
+
+    printf("%d", fat);
+    return (0);
+}
